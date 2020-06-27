345e131a8a23d3e4de537355ce53a2ec90ec4b14
Mwansa <Mwansad.mwango@gmail.com>
2020-06-27 23:51:53 +0800

    debug

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-// Database Connection Request
-require('dotenv/config');
-const connectDB = require("./config/connectDB.js");
-
-// const PORT = 5000;
+const PORT = 5000;
 
 const app = express();
 
@@ -19,12 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 9090;
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true });
 
 // routes
 app.use(require("./routes/api.js"));
-connectDB()
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}! -- http://localhost:${PORT}/`);
 });
