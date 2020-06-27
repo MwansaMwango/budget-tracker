5027ddac96d38f9c350699d817d97957d705eb64
Mwansa <Mwansad.mwango@gmail.com>
2020-06-27 21:53:40 +0800

    Edited commentry

--- public/index.js
+++ public/index.js
@@ -136,7 +136,7 @@ function sendTransaction(isAdding) {
     }
   })
   .catch(err => {
-    // fetch failed, so save in indexed db // TODO Save in cache instead?
+    // fetch failed, so save in indexed db 
     console.log("Fetch failed so saveRecord in indexedDB");
     saveRecord(transaction);
 
